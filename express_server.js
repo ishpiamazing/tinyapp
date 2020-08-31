975ed6b1a7356e0432a034b7b40a71898256437d
ishpiamazing <ishpiamazing@gmail.com>
2020-08-31 22:41:24 +0000

    body-parser added

--- express_server.js
+++ express_server.js
@@ -2,14 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-//  tells the Express app to use EJS as its templating engine
+function generateRandomString() {
 
+  const id = Math.random().toString(36).substring(2,8);
+}
+
+
+//  tells the Express app to use EJS as its templating engine
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//Adding middleware to convert data into human readable-form
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,16 +33,26 @@ app.get("/hello", (req, res) => {
 });
 
 // Addding a route for /urls
-
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
+//Add a GET Route to Show the Form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //Adding a Second Route and Template
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+//Add a POST Route to Receive the Form Submission
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
